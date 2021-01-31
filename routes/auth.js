89754daf555f870c816f1443d4df5e1d0da68629
Alex-Vel <t9veal00@students.oamk.fi>
2021-01-31 15:07:58 +0200

    added auth paths

--- routes/auth.js
+++ routes/auth.js
@@ -3,38 +3,66 @@ const router = express.Router();
 const db = require('../db/index');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
-const passportHttp = require('passport-http');
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy,
+      ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwtSecretKey = require('./jwt-key.json');
 
 
-passport.use(new passportHttp.BasicStrategy((email, password, done) => {
-  db.query('SELECT * FROM users WHERE email=$1', [email])
-    .then(user => {
+//JWT auths
 
-      if (user.rows.length === 0) return done(null, false);
 
-      const validPassword = bcrypt.compareSync(password, user.rows[0].password);
-      if (!validPassword) return done(null, false);
+let options = {}
 
-      done(null, user.rows);
-    })
-})
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+
+options.secretOrKey = jwtSecretKey.secretKey;
+
+passport.use(new JwtStrategy(options, function(jwt_payload, done) {
+  console.log("Processing JWT payload for token content:");
+  console.log(jwt_payload);
+
+   done(null, jwt_payload.user);
+
+}));
+
+
+app.get(
+  '/jwtProtectedResource',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    console.log("jwt");
+    res.json(
+      {
+        status: "Successfully accessed protected resource with JWT",
+        user: req.user
+      }
+    );
+  }
 );
 
-// login a user
-router.post('/', (req, res, next) => {
-  passport.authenticate('basic', (err, user, info) => {
-    if (err) return next(err);
-    if (!user) return res.status(400).send('Invalid email or password.');
 
-    const { user_id, name, email } = user[0];
-    const userInfo = {
-      user_id,
-      name,
-      email
+app.get(
+  '/loginForJWT',
+  passport.authenticate('basic', { session: false }),
+  (req, res) => {
+    const body = {
+      id: req.user.id,
+      email : req.user.email
+    };
+
+    const payload = {
+      user : body
+    };
+
+    const options = {
+      expiresIn: '1d'
     }
 
-    res.send(userInfo);
-  })(req, res, next);
-});
+
+    const token = jwt.sign(payload, jwtSecretKey.secretKey, options);
+
+    return res.json({ token });
+})
 
 module.exports = router;
