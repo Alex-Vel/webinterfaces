b1582629b37e4247293491aff8bc24b20a9fef9a
Alex-Vel <t9veal00@students.oamk.fi>
2021-02-04 23:02:40 +0200

    smaller changes

--- server.js
+++ server.js
@@ -1,59 +1,40 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const fs = require("fs");
-const Ajv = require("ajv").default;
 
 const app = express();
 const port = 4000;
 
-//routes
+//init routes
 const users = require("./routes/users");
 const postings = require("./routes/postings");
-
-const imageUpload = require("./components/imageUpload");
-
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
-app.use("/fileUpload", imageUpload);
-
+//use the two routes
 app.use("/users", users);
 app.use("/postings", postings);
 
 app.get("/", (req, res) => {
   res.status(200);
-  res.send("Hello World!");
+  res.send("This is the API for an online market");
 });
 
-app.post("/test", (req, res) => {
-  console.log(req.body);
-
-  const ajv = new Ajv();
-  const validate = ajv.compile(jsonSchemaDocument);
-  const valid = validate(req.body);
-  if (valid == true) {
-    res.status(200);
-    res.send("OK");
-  } else {
-    res.status(400);
-    console.log(validate.errors);
-    res.send(validate.errors.map((e) => e.message));
-  }
-});
-
+//initial server instance is null
 let serverInstance = null;
 
+//assign an instance to the serverinstance var
 module.exports = {
   start: function () {
     serverInstance = app.listen(port, () =>
+      //console log server start if all is good
       console.log(`Server is up and listening on ${port}...`)
     );
   },
   close: function () {
+    //close server for tests
     serverInstance.close();
   },
 };
