a9083608824d1c1b055596d6f2736b0d9ac7af2c
Alex-Vel <t9veal00@students.oamk.fi>
2021-02-17 17:03:28 +0200

    PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const cors = require("cors");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //init routes
 const users = require("./routes/users");
